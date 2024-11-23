d3b750f38f3bb15663cb1d2822cb60f9e2e37499
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2024-11-23 20:35:45 +0100

    implement refreshing tokens

--- website/Account/TokenAuthenticationService.cs
+++ website/Account/TokenAuthenticationService.cs
@@ -1,31 +1,66 @@
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Security.Claims;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Authorization;
 
-namespace website.Account {
+namespace website.Account
+{
     public class TokenAuthenticationService
     {
         private readonly HttpClient httpClient;
 
-        public TokenAuthenticationService(HttpClient httpClient) {
+        public TokenAuthenticationService(HttpClient httpClient)
+        {
             this.httpClient = httpClient;
         }
 
-        public async Task<TokenAuthenticationResponse?> Login(TokenAuthenticationRequest request) {
-            try {
+        public async Task<TokenAuthenticationResponse?> Login(TokenAuthenticationRequest request)
+        {
+            try
+            {
                 var result = await httpClient.PostAsJsonAsync("api/users/login", request);
 
-                if (result.IsSuccessStatusCode) {
+                if (result.IsSuccessStatusCode)
+                {
                     using var contentStream = await result.Content.ReadAsStreamAsync();
                     var response = await JsonSerializer.DeserializeAsync<TokenAuthenticationResponse>(contentStream);
 
                     return response;
                 }
 
-            } catch(Exception e) {
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return null;
+        }
+
+        internal async Task<TokenAuthenticationResponse?> Refresh(string token)
+        {
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Post,"api/users/refresh-token");
+
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                using var response = await httpClient.SendAsync(request);
+                                
+                if (response.IsSuccessStatusCode)
+                {
+                    using var contentStream = await response.Content.ReadAsStreamAsync();
+                    var result = await JsonSerializer.DeserializeAsync<TokenAuthenticationResponse>(contentStream);
+
+                    return result;
+                }
+
+            }
+            catch (Exception e)
+            {
                 Console.WriteLine(e);
             }
 
--- website/Account/TokenAuthenticationStateProvider.cs
+++ website/Account/TokenAuthenticationStateProvider.cs
@@ -3,56 +3,108 @@ using Microsoft.AspNetCore.Components.Authorization;
 using System.IdentityModel.Tokens.Jwt;
 using Microsoft.AspNetCore.Components;
 
-namespace website.Account {
+namespace website.Account
+{
     public class TokenAuthenticationStateProvider : AuthenticationStateProvider
     {
         private ClaimsIdentity identity = new ClaimsIdentity();
         private Blazored.LocalStorage.ISyncLocalStorageService localStorage;
+        private TokenAuthenticationService tokenAuthenticationService;
 
-        public TokenAuthenticationStateProvider(Blazored.LocalStorage.ISyncLocalStorageService localStorage) {
+        public TokenAuthenticationStateProvider(
+            Blazored.LocalStorage.ISyncLocalStorageService localStorage,
+            TokenAuthenticationService tokenAuthenticationService)
+        {
             this.localStorage = localStorage;
+            this.tokenAuthenticationService = tokenAuthenticationService;
         }
-        
+
         public async override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            var token = localStorage.GetItemAsString("token");            
+            var token = localStorage.GetItemAsString("token");
             identity = GetIdentityFromToken(token ?? "");
 
             return new AuthenticationState(new ClaimsPrincipal(identity));
         }
 
-        public void StateFromResponse(TokenAuthenticationResponse response) {
-            var expires = DateTimeOffset.FromUnixTimeSeconds(response.Exp);
-
-            if (expires > DateTime.Now && !string.IsNullOrEmpty(response.Token)) {
-                identity = GetIdentityFromToken(response.Token);
+        private async Task RefreshToken(string? token)
+        {
+            if (string.IsNullOrEmpty(token))
+            {
+                return;
             }
+
+            JwtSecurityToken? jwtToken = ReadTokenAsJwt(token);
+
+            if (jwtToken != null)
+            {
+                if (jwtToken.ValidTo > DateTime.Now &&
+                                jwtToken.ValidTo.AddMinutes(5) <= DateTime.Now)
+                {
+                    var authResponse = await tokenAuthenticationService.Refresh(token);
+
+                    if (authResponse != null)
+                    {
+                        StateFromResponse(authResponse);                        
+                        await Task.Delay(jwtToken.ValidTo.AddMinutes(-5) - DateTime.Now);
+                        await RefreshToken(localStorage.GetItemAsString("token"));
+                    }
+                }
+            }
+
+            localStorage.RemoveItem("token");
         }
 
-        public ClaimsIdentity GetIdentityFromToken(string token) {
-            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+        public async void StateFromResponse(TokenAuthenticationResponse response)
+        {
+            var expires = DateTimeOffset.FromUnixTimeSeconds(response.Exp);
 
-            if(tokenHandler.CanReadToken(token)) {
-                JwtSecurityToken jwtToken = tokenHandler.ReadJwtToken(token);
+            if (expires > DateTime.Now && !string.IsNullOrEmpty(response.Token))
+            {
+                identity = GetIdentityFromToken(response.Token);
+                await RefreshToken(response.Token);
+            } else {
+                localStorage.RemoveItem("token");
+            }
+            
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
 
-                if (jwtToken.ValidTo > DateTime.Now) {
-                    identity = new ClaimsIdentity(
-                        new List<Claim>(){
+        public ClaimsIdentity GetIdentityFromToken(string token)
+        {
+            JwtSecurityToken jwtToken = ReadTokenAsJwt(token);
+
+            if (jwtToken != null && jwtToken.ValidTo > DateTime.Now)
+            {
+                identity = new ClaimsIdentity(
+                    new List<Claim>(){
                             new Claim(ClaimTypes.Sid, jwtToken.Claims.FirstOrDefault(claim => claim.Type == "id")?.Value ?? ""),
                             new Claim(ClaimTypes.Actor, jwtToken.Claims.FirstOrDefault(claim => claim.Type == "email")?.Value ?? ""),
                             new Claim(ClaimTypes.Name, jwtToken.Claims.FirstOrDefault(claim => claim.Type == "email")?.Value ?? ""),
                             new Claim(ClaimTypes.Role, "User"),
-                        },
-                        "Payload JWT"                            
-                    );
-                    
-                    localStorage?.SetItemAsString("token", token);
+                    },
+                    "Payload JWT"
+                );
 
-                    return identity;
-                }
+                localStorage?.SetItemAsString("token", token);
+
+                return identity;
             }
 
+
             return new ClaimsIdentity();
+        }
+
+        private JwtSecurityToken? ReadTokenAsJwt(string? token)
+        {
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+
+            if (tokenHandler.CanReadToken(token))
+            {
+                return tokenHandler.ReadJwtToken(token);
+            }
+
+            return null;
         }
     }
 }
